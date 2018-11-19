ab06afb55b2ac933edeace5f598396e6de7eddc4
Anthony Garcia <garcia.anthony577@gmail.com>
2018-11-18 21:13:31 -0700

    attempting to add shuffle function

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ function changeColor(newColor) {
 	var para1=document.getElementById('pickle');
 	para1.style.color = newColor;
 }
+
+function shufflize() {
+	var txt = document.getElementById("shuffle").textContent;
+	var words = txt.split(" ");
+	words.map(function(t) {
+		for(i = (wordds.length - 1); i > 0; i--) {
+			var j = Math.floor(Math.random() * (i + 1));
+			var temp = words[i];
+			word[i] = words[j];
+			words[j] = temp;
+		}
+		document.getElementById("shuffle").textContent = words.join(" ");
+	});
+}
